af851b89c47ab942f4f9497372c332479e5a4f81
camelenchon <camelenchon@hotmail.com>
2024-05-21 23:41:29 -0300

    Lista 5 - Primeiros passos de JS

---
+++ Listas/Lista 5/Exercicio1/js/exercicio1.js
@@ -0,0 +1,7 @@
+var nome = prompt('Digite seu nome')
+var idade = prompt('Digite sua idade')
+var empregoAtual = prompt('Qual seu emprego atual')
+var endereco = prompt('Qual seu endereço')
+
+alert(`Seu nome é ${nome} \nVocê tem ${idade} anos \nSeu emprego atual é: ${empregoAtual} \nVocê mora no ${endereco}`)
+
---
+++ Listas/Lista 5/Exercicio2/js/exercicio2.js
@@ -0,0 +1,13 @@
+var nome = "Maria"
+var idade = 25
+var empregoAtual = "Florista"
+var endereco = "Rua das Casas, 30"
+
+console.log("Seu nome é " + nome)
+console.log("Sua idade é: " + idade)
+console.log("Seu emprego atual é: " + empregoAtual)
+console.log("Seu endereço é: " + endereco)
+
+
+
+ 
